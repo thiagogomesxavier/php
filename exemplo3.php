c605aa73ffc71541b507be8ea2087c95663a4ca4
thiagogomesxavier <49047892+thiagogomesxavier@users.noreply.github.com>
2019-06-19 08:18:29 -0300

    TIPOS DE VARIÁVEIS

---
+++ exemplo3.php
@@ -0,0 +1,51 @@
+<?php
+
+//TIPO BÁSICOS
+
+// STRING
+$nome = "Thiago";
+
+// STRING COM ASPAS SIMPLES
+$facebook = 'thiagoxfotografia';
+
+// NUMÉRICO
+$ano= 1990;
+
+// PONTO FLUTUANTE
+$salario= 5500.99;
+
+// BOLEANO
+$bloqueado= false;
+////////////////////////////////////////////
+
+
+// TIPOS COMPOSTOS
+
+//ARRAY
+$frutas = array("abacaxi", "laranja", "manga");
+
+echo $frutas[2];
+
+//ORIENTAÇÂO OBJETO
+$nascimento= new DateTime();
+
+//var_dump($nascimento);
+
+//////////////////////////////////////////////
+
+// TIPOS ESPECIAIS
+$arquivo= fopen("exemplo3.php", "r");
+
+//var_dump($arquivo);
+
+// Nulo é ausencia de valor
+$nulo= NULL;
+
+// Espaço vazio
+$nulo= "";
+
+
+
+
+
+?>
